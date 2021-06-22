b4f4866486707e5fe10360143e7632032635f6eb
Nobody4931 <ardenxie1@gmail.com>
2021-06-21 20:48:36 -0400

    stealing code from others pt 3 10% of a daily programmer's time is spent coding

--- src/main.cpp
+++ src/main.cpp
@@ -7,43 +7,58 @@
  * fmt api docs: https://fmt.dev/
  * spdlog api docs: https://github.com/gabime/spdlog/wiki/1.-QuickStart
  * json api docs: https://nlohmann.github.io/json
+ * asio api docs: https://think-async.com/Asio/asio-1.18.2/doc/
  *
  */
 
-#include "spdlog/spdlog.h"
-#include "nlohmann/json.hpp"
+#define ASIO_STANDALONE
 
-using json = nlohmann::json;
+#include <cstring>
+#include <vector>
+
+#include <spdlog/spdlog.h>
+#include <nlohmann/json.hpp>
+#include <asio.hpp>
+
+inline void check_asio_error( const asio::error_code& error, const char* message ) {
+	if ( error ) {
+		spdlog::error( message, error.message() );
+		throw asio::system_error( error );
+	}
+}
 
 int main() {
 	// make sure this stuff works
-	spdlog::set_level( spdlog::level::debug );
-	spdlog::set_pattern( "[%m.%d.%C - %I:%M:%S] [%^%l%$] %v" );
+	asio::io_context context;
+	asio::error_code error;
 
-	const char* random_json_str = R"(
-	{
-		"value_1": "sample text",
-		"value_2": 69,
-		"value_3": 4.20,
-		"value_4": true,
-		"value_5": null,
-		"value_6": {
-			"subvalue_1": [
-				"str",
-				123,
-				4.56,
-				false,
-				null
-			]
-		}
-	}
-	)";
+	asio::ip::tcp::resolver resolver( context );
+	asio::ip::tcp::socket socket( context );
 
-	auto random_json = json::parse( random_json_str );
-	for ( auto& item : random_json.items() ) {
-		spdlog::debug( "Key: {}", item.key() );
-		spdlog::debug( "Typ: {}", item.value().type_name() );
-		spdlog::debug( "Val: {}", item.value().dump() );
-		spdlog::debug( "==========================" );
+	socket.connect( resolver.resolve( "httpbin.org", "80" )->endpoint(), error );
+	check_asio_error( error, "Failed to connect: {}" );
+	spdlog::info( "Connected to httpbin.org" );
+
+	const char* payload =
+		"GET /ip HTTP/1.1\r\n"
+		"Host: httpbin.org\r\n"
+		"Connection: close\r\n"
+		"\r\n";
+	const size_t payload_s = strlen( payload );
+	const size_t buffer_s = 1024;
+	char buffer[ buffer_s ];
+
+	socket.write_some( asio::buffer( payload, payload_s ), error );
+	check_asio_error( error, "Failed to send payload: {}" );
+	spdlog::info( "Sent payload to /ip" );
+
+	socket.wait( socket.wait_read );
+
+	while ( true ) {
+		size_t bytes = socket.read_some( asio::buffer( buffer, buffer_s ), error );
+		if ( error == asio::error::eof ) break;
+		else check_asio_error( error, "An error occurred: {}" );
+
+		spdlog::info( "Received {} bytes:\n{:.{}}", bytes, buffer, bytes );
 	}
 }
